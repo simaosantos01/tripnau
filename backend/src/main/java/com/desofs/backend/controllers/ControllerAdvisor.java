9b831452a34aa330deb0230aeaa89a9e77d55065
nunoribeiro-1230201 <1230201@isep.ipp.pt>
2024-05-16 02:39:57 +0100

    #24 backend controller advisor interceptor

---
+++ backend/src/main/java/com/desofs/backend/controllers/ControllerAdvisor.java
@@ -0,0 +1,106 @@
+package com.desofs.backend.controllers;
+
+import com.desofs.backend.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class ControllerAdvisor {
+
+    @Data
+    public static class ErrorPayloadMessage {
+        private LocalDateTime timestamp;
+        private int status;
+        private String error;
+        private String message;
+        private String path;
+    }
+
+    private String getPath(WebRequest request) {
+        if (request instanceof ServletWebRequest) {
+            HttpServletRequest servletRequest = ((ServletWebRequest) request).getRequest();
+            return servletRequest.getRequestURI();
+        }
+        return "";
+    }
+
+    private ErrorPayloadMessage createPayload(String message, WebRequest request) {
+        ErrorPayloadMessage errorResponse = new ErrorPayloadMessage();
+        errorResponse.setTimestamp(LocalDateTime.now());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setError(HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errorResponse.setMessage(message);
+        errorResponse.setPath(this.getPath(request));
+        return errorResponse;
+    }
+
+    private ErrorPayloadMessage createPayload(Exception exception, WebRequest request) {
+        return createPayload(exception.getMessage(), request);
+    }
+
+    private ErrorPayloadMessage createPayload(Exception exception, WebRequest request, String msg) {
+        ErrorPayloadMessage errorResponse = this.createPayload(exception, request);
+        errorResponse.setMessage(msg);
+        return errorResponse;
+    }
+
+    private static String mapToString(Map<String, String> map) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append("=");
+            sb.append(entry.getValue());
+            sb.append(", ");
+        }
+
+        if (!map.isEmpty()) {
+            sb.setLength(sb.length() - 2);
+        }
+
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleInvalidsArguments(MethodArgumentNotValidException exception, WebRequest request) {
+        Map<String, String> errorMap = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errorMap.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(this.createPayload(exception, request, mapToString(errorMap)));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleIllegalArgumentExceptions(NotFoundException exception, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(this.createPayload(exception, request));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentExceptions(IllegalArgumentException exception, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(this.createPayload(exception, request));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericExceptions(Exception exception, WebRequest request) {
+        log.error("Not caught exception: {}", exception.getMessage());
+        String genericMsg = "Some generic error occurred. Contact administrator.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(this.createPayload(genericMsg, request));
+    }
+
+}
