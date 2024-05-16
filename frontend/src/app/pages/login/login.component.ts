aa1223165d101df211e399c0f106ffa66e1fc313
Domingos Machado <1190535@isep.ipp.pt>
2024-05-16 22:24:31 +0100

    Login Component

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,87 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ROUTE } from '../../enum/routes';
+import { LoginRequest } from '../../model/login-request';
+import { LoginResponse } from '../../model/login-response';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CardModule, ButtonModule, PasswordModule, InputTextModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  fb = inject(FormBuilder)
+  authService = inject(AuthService)
+  router = inject(Router)
+
+  form = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required]
+  });
+
+  // timeouts in milliseconds for when login fails
+  timeouts: number[] = [30 * 1000, 60 * 1000, 300 * 1000, 900 * 1000, 3600 * 1000];
+  timeoutsString: string[] = ['30  seconds', '1 minute', '5 minutes', '15 minutes', '1 hour']
+  timeoutString: string = '';
+
+  // individual failed attempt
+  failedAttempts: number = 0;
+  // total attempts (number of times the one above was equal to three)
+  totalFailedAttempts: number = 0;
+
+  // if the login button should be disabled
+  disabled: boolean = false;
+
+  constructor() { }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.form.setErrors({});
+      const credentials: LoginRequest = {
+        email: this.form.controls.email.value!,
+        password: this.form.controls.password.value!
+      }
+      this.authService.login(credentials).subscribe({
+        next: (response: LoginResponse) => this.handleLoginResponse(response),
+        error: (error: HttpErrorResponse | HttpResponse<LoginResponse>) => this.handleError(error)
+      })
+    }
+  }
+
+  handleLoginResponse(response: LoginResponse) {
+    if (response.success) {
+      this.router.navigateByUrl(ROUTE.LANDING);
+    } else {
+      this.failedAttempts += 1;
+      if (this.failedAttempts === 3) {
+        // define the timeout function such that it now starts
+        this.disabled = true;
+        setTimeout(() => {
+          this.form.setErrors({ tooManyFailures: true })
+          this.disabled = false;
+        }, this.timeouts[this.totalFailedAttempts])
+        this.failedAttempts = 0;
+        this.totalFailedAttempts++;
+      } else {
+        this.form.setErrors({ invalidCredentials: true })
+      }
+    }
+  }
+
+  handleError(error: any) {
+    if (error.name) {
+      this.form.setErrors({ backendfault: true });
+    } else {
+      this.form.setErrors({ tooManyRequests: true });
+    }
+  }
+}
