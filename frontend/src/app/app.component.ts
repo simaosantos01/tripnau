f1c4aabaac8699606b6fdc23c314824e999abe1d
Domingos Machado <1190535@isep.ipp.pt>
2024-05-16 20:29:11 +0100

    Auth guard
    Adding auth guard and experimental routes.
    Adjusting auth service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  authService = inject(AuthService);
+
+  ngOnInit(): void {
+    if (localStorage.getItem('token')) {
+      this.authService.setToken(localStorage.getItem('token')!);
+    }
+  }
 }
