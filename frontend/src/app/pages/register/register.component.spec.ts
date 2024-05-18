07e217ae6b85aa2a527403caa9ed73a6eaaedd34
Domingos Machado <1190535@isep.ipp.pt>
2024-05-18 13:28:26 +0100

    Register Page
    Adding register page.
    Improving error/success messages
    Adding production environment
    Adding husky dependency to force test run before push

--- frontend/src/app/pages/register/register.component.spec.ts
+++ frontend/src/app/pages/register/register.component.spec.ts
@@ -2,28 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { ReactiveFormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
+import { RegisterComponent } from './register.component';
 import { routes } from '../../app.routes';
-import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent, ButtonModule, PasswordModule, InputTextModule, ReactiveFormsModule],
+      imports: [RegisterComponent],
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
         provideRouter(routes),
       ]
-    }).compileComponents();
-    fixture = TestBed.createComponent(LoginComponent);
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -32,3 +30,4 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
