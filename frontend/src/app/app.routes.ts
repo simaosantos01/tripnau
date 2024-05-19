ea103018d7e19daaea062af15477d06e5fbd3a4a
Afonso Machado <1190326@isep.ipp.pt>
2024-05-19 18:38:53 +0100

    Fixes to work in dev

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { authGuard } from './guards/auth.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BookingComponent } from './pages/booking/booking.component';
+//import { BookingComponent } from './pages/booking/booking.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [authGuard([])] },
