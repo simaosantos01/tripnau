aae51b1468509ea8210e83561bd32628473d741d
Afonso Machado <1190326@isep.ipp.pt>
2024-05-19 19:22:04 +0100

    Fix spec class

--- frontend/src/app/services/review.service.spec.ts
+++ frontend/src/app/services/review.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReviewService } from './review.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ReviewService', () => {
   let service: ReviewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]});
     service = TestBed.inject(ReviewService);
   });
 
