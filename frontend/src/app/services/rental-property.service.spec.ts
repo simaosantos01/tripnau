84cad698ec277cd4687f58313a81152b3bfaf04b
Afonso Machado <1190326@isep.ipp.pt>
2024-05-19 18:05:05 +0100

    FIx to spec.ts

--- frontend/src/app/services/rental-property.service.spec.ts
+++ frontend/src/app/services/rental-property.service.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RentalPropertyService } from './rental-property.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('RentalPropertyService', () => {
   let service: RentalPropertyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]});
     service = TestBed.inject(RentalPropertyService);
   });
 
