08a2044be9b319d02c4ed9e10626213abab9601b
Afonso Machado <1190326@isep.ipp.pt>
2024-06-02 11:16:55 +0100

    User page
    Allows the user to change their information, change password and also logout. Missing backend endpoints

--- frontend/src/app/enum/routes.ts
+++ frontend/src/app/enum/routes.ts
@@ -1,9 +1,11 @@
 export enum ROUTE {
     LOGIN = '/login',
+    LOGOUT = '/logout',
     REGISTER = '/register',
     LANDING = '/',
     RENTALPROPERTIES = '/rental_property/all',
     RENTALPROPERTIESBYUSER = '/rental_property/allByUser',
     BOOKPROPERTY = '/booking/add',
-    USER = '/user'
+    USER = '/user',
+    USERBYEMAIL = '/userEmail',
 }
