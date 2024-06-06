09d90c684bb98e3dd6753b70469a84aab9016ebc
nunoribeiro-1230201 <1230201@isep.ipp.pt>
2024-06-06 16:00:02 +0100

    #94, #95 HTTPS, CORS and CSRF configuration

--- backend/src/main/java/com/desofs/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/desofs/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.desofs.backend.config;
 import com.desofs.backend.domain.enums.Authority;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +20,14 @@ import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthen
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -40,46 +50,52 @@ public class SecurityConfig {
 
         String rentalPropertyEndpoint = "/rental_property/{id}";
 
-        http = http.cors().and().csrf().disable();
+        http.cors(corsSpec -> {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowCredentials(true);
+            corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
+            List.of("*").forEach(corsConfig::addAllowedMethod);
+            List.of("*").forEach(corsConfig::addAllowedHeader);
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfig);
+            corsSpec.configurationSource(source);
+        });
 
-        http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+        http.csrf(AbstractHttpConfigurer::disable);
 
-        http.exceptionHandling(exceptions ->
-                exceptions
-                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
+        http.sessionManagement(sessionSpec -> sessionSpec.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.exceptionHandling(exceptions -> {
+            exceptions.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint());
+            exceptions.accessDeniedHandler(new BearerTokenAccessDeniedHandler());
+        });
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
         http.addFilterAfter(ipFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers(HttpMethod.POST, "/auth/login/generateOTP").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-
                 .requestMatchers(HttpMethod.GET, "/rental_property/all").permitAll()
                 .requestMatchers(HttpMethod.POST, rentalPropertyEndpoint).permitAll()
                 .requestMatchers(HttpMethod.POST, "/rental_property/create").hasAuthority(Authority.PROPERTYOWNER)
-
                 .requestMatchers(HttpMethod.GET, rentalPropertyEndpoint).permitAll()
                 .requestMatchers(HttpMethod.POST, "/rental_property/create").hasAuthority(Authority.PROPERTYOWNER)
                 .requestMatchers(HttpMethod.GET, "/rental_property/allByUser/{id}").hasAnyAuthority(Authority.PROPERTYOWNER, Authority.BUSINESSADMIN)
                 .requestMatchers(HttpMethod.DELETE, rentalPropertyEndpoint).hasAnyAuthority(Authority.PROPERTYOWNER, Authority.BUSINESSADMIN)
                 .requestMatchers(HttpMethod.PUT, rentalPropertyEndpoint).hasAnyAuthority(Authority.PROPERTYOWNER, Authority.BUSINESSADMIN)
-
                 .requestMatchers(HttpMethod.POST, "/booking/add").permitAll()
                 .requestMatchers(HttpMethod.POST, "/booking/stripe-webhook").permitAll()
                 .requestMatchers(HttpMethod.POST, "/booking/{id}").authenticated()
                 .requestMatchers(HttpMethod.POST, "/booking/getAllByUser/{id}").authenticated()
                 .requestMatchers(HttpMethod.PUT, "/booking/{id}/cancel").authenticated()
-
                 .requestMatchers(HttpMethod.POST, "/review/add").permitAll()
                 .requestMatchers(HttpMethod.POST, "/review/change_state").hasAuthority(Authority.BUSINESSADMIN)
-
                 .requestMatchers(restApiDocPath + "/**").permitAll()
                 .requestMatchers(swaggerPath + "/**").permitAll()
-                .anyRequest().authenticated());
+                .anyRequest().authenticated()
+        );
 
         return http.build();
     }
@@ -88,18 +104,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        final CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
